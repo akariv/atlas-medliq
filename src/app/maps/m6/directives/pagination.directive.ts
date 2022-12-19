020c6a60d429a140307f83166c0044abba8b6426
jilalat <ae.jilalat@pyxicom.com>
2022-12-19 12:12:57 +0100

    remove jq from synchronized thumbnails

--- src/app/maps/m6/directives/pagination.directive.ts
+++ src/app/maps/m6/directives/pagination.directive.ts
@@ -1,40 +1,29 @@
-import { Directive,ElementRef,HostListener } from '@angular/core';
-import * as $ from 'jquery';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appPagination]'
+  selector: '[appPagination]',
 })
 export class PaginationDirective {
-
-  constructor(private el: ElementRef) {
-  }
+  constructor(private el: ElementRef) {}
   @HostListener('click')
-  pagination(){
+  pagination() {
     var elm = this.el.nativeElement;
-    Array.from(document.querySelectorAll('.list li.active')).forEach(function(elem) {
+    Array.from(document.querySelectorAll('.list li.active')).forEach(function (
+      elem
+    ) {
       elem.classList.remove('active');
     });
-    Array.from(document.querySelectorAll('.thumbnail-slick')).forEach(function(elem) {
+    Array.from(document.querySelectorAll('.thumbnail-slick')).forEach(function (
+      elem
+    ) {
       elem.classList.remove('active');
     });
-    // .classList.remove('active');
     elm.classList.add('active');
     var index = elm.getAttribute('data-index');
-    // $(".thumbnail-slick[data-index="+ index +"]").addClass('active');
-    // if(index == $('.list li').length - 1){
-    //   $('.arrow-next').addClass('disaple');
-    //   $('.arrow-pre').removeClass('disaple');
-    // }
-    // if(index == 0){
-    //   $('.arrow-pre').addClass('disaple');
-    //   $('.arrow-next').removeClass('disaple');
-    // }
-    // if(index != 0 && index != $('.list li').length - 1 ){
-    //   $('.arrow-pre').removeClass('disaple');
-    //   $('.arrow-next').removeClass('disaple');
-    // }
 
-    const thumbnailSlick = document.querySelector(`.thumbnail-slick[data-index="${index}"]`);
+    const thumbnailSlick = document.querySelector(
+      `.thumbnail-slick[data-index="${index}"]`
+    );
     thumbnailSlick.classList.add('active');
 
     const arrowNext = document.querySelector('.arrow-next');
